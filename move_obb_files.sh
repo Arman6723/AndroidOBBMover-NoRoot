0e2e1abe26c1564b590c4f6c883f04c27534a2f0
𝔄𝔯𝔪𝔞𝔫 𝔖 <98284201+Arman6723@users.noreply.github.com>
2024-03-04 21:27:54 -0500

    Update move_obb_files.sh

--- move_obb_files.sh
+++ move_obb_files.sh
@@ -1,22 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-# Define the source and destination directories
-SOURCE_DIR="/path/to/source/directory"
-DEST_DIR="/path/to/destination/directory"
+# Define your source and destination paths for file OR folder
+# Each source[i] corresponds to destination[i]
+source[0]="/path/to/source/directory1"
+destination[0]="/path/to/destination/directory1"
 
-# Get a list of .obb files in the source directory
-FILES=$(adb shell ls $SOURCE_DIR/*.obb)
+# Uncomment the following lines to move another file/folder
 
-# Loop through each file and move it to the destination directory
-for FILE in $FILES
-do
-  echo "Moving $FILE to $DEST_DIR"
-  adb shell mv $FILE $DEST_DIR
+# source[1]="/path/to/source/file2"
+# destination[1]="/path/to/destination/directory2"
+
+# source[2]="/path/to/source/directory"
+# destination[2]="/path/to/destination/directory"
+
+# Get the length of the arrays
+length=${#source[@]}
+
+# Use a loop to move each file from its source to its destination
+for ((i=0; i<$length; i++)); do
+  adb push ${source[$i]} ${destination[$i]}
 done
-
-echo "All .obb files have been moved from $SOURCE_DIR to $DEST_DIR"
-
-
-# Please replace "/path/to/source/directory" and "/path/to/destination/directory" with the actual paths on your Android device.
-
-# To run this script, save it to a file named move_obb_files.sh, open a terminal or command prompt, navigate to the directory where you saved the script, and type ./move_obb_files.sh.
